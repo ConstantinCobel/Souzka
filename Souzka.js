7c6f48c38f5eadb8434359f8c653d759255e2150
ConstantinCobel <146664224+ConstantinCobel@users.noreply.github.com>
2023-10-02 22:24:42 +0300

    Add files via upload

--- Souzka.js
+++ Souzka.js
@@ -2,32 +2,37 @@
 
                   //    Кнопка ВХОД
     document.querySelector('#Open').onclick = function () {
-        var Souz = document.querySelector('#Souz').value;
-        var Nik = document.querySelector('#Nik').value;
-        var Cod = document.querySelector('#Cod').value;
-		if (document.getElementById("Souz").value == "ДА") {
-		  document.getElementById("Souz").value="НЕТ";
-		} 
-		else 
-		{
-		  document.getElementById("Souz").value="ДА";
-		//document.getElementById("Pan0").style.display = "none";
-		};	
+        var MyUnit = document.querySelector('#MyUnit').value;
+        var MyAccaunt = document.querySelector('#MyAccaunt').value;
+		//alert("Итог: " + MyUnit + " " + MyAccaunt);
 		//document.getElementById("Souz").value=Souz;
-		//document.getElementById("Cod").value="NO";
+		//document.getElementById("Nik").value="NO";
+		//document.getElementById("Pan0").style.display = "none";
 		opulate();
+	
     }; 
 
-
+                   // Получение Souzka.json в виде оюъекта
 function opulate() {
 var equestURL ="https://constantincobel.github.io/Souzka//Souzka.json";
 var request = new XMLHttpRequest();
-request.open("GET", equestURL);
-request.responseType = "json";
-request.send();
-request.onload = function () {
-  var superHeroes = request.response;
-document.getElementById("Cod").value=superHeroes["squadName"];
-};
+  request.open("GET", equestURL);
+  request.responseType = "json";
+  request.send();
+  request.onload = function () {
+    var superHeroes = request.response;
+	var text1 = superHeroes["squadName"];
+    document.getElementById("Pan0").style.display = "none";
+	alert("Итог: " + superHeroes);
+  };
 
 };
+
+function mouseoverPass() {
+  let obj = document.getElementById('MyAccaunt');
+  obj.type = 'text';
+}
+function mouseoutPass() {
+  let obj = document.getElementById('MyAccaunt');
+  obj.type = 'password';
+}
