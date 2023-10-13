590b46f2b3b70a5ffa23e86064fc600d224a75bd
ConstantinCobel <146664224+ConstantinCobel@users.noreply.github.com>
2023-10-13 03:16:23 +0300

    Update json.php

--- json/json.php
+++ json/json.php
@@ -1,11 +1,8 @@
 <?php
 // на какие данные рассчитан этот скрипт
-Header set Access-Control-Allow-Origin "*"
-Header set Access-Control-Allow-Methods: "GET,POST,OPTIONS,DELETE,PUT"
-Header add Access-Control-Allow-Headers "origin, x-requested-with, content-type"
 header("Content-Type: application/json");
 // разбираем JSON-строку на составляющие встроенной командой
 $data = json_decode(file_get_contents("php://input"));
 // отправляем в ответ строку с подтверждением
-echo "Сервер получил данные:";
+echo "Сервер получил следующие данные: имя — $data->name, фамилия — $data->lastname";
 ?>
